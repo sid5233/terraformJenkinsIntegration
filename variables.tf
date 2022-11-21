537c9150e6fc85894f3223457724383c12c8621a
Sudarshan Bhamare <sudarshan.bhamare@gmail.com>
2022-11-21 12:19:24 +0530

    string error

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "vpc_cidr" {
   description = "Choose cidr for VPC"
-  type = "string"
+  type = string
   default = "10.20.0.0/16"
 }
 
 variable "region" {
   description = "choose regin stack"
-  type = "string"
+  type = string
   default = "us-east-1"
 }
 
