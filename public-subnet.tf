8ac30d778f73f0cd10513b08d4df0b60e51691cb
sid5233 <sudarshan.bhamare@gmail.com>
2022-11-22 11:55:05 +0530

    pub subnet, igw, rt, rt-assc added.

--- public-subnet.tf
+++ public-subnet.tf
@@ -1,5 +1,7 @@
 locals {
   az_names = "${data.aws_availability_zones.azs.names}"
+  pub_sub_id = "${aws_subnet.public.*.id}"
+
 }
 
 resource "aws_subnet" "public" {
@@ -10,4 +12,24 @@ resource "aws_subnet" "public" {
   tags = {
     "Name" = "Public-Subnet-${count.index + 1}"
   }
+}
+
+resource "aws_internet_gateway" "tf-igw" {
+    vpc_id = "${aws_vpc.my_tf_app.id}"
+
+}
+
+resource "aws_route_table" "public-RT" {
+      vpc_id = "${aws_vpc.my_tf_app.id}"
+
+      route = {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.tf-igw.id}"
+      }
+}
+
+resource "aws_route_table_association" "pub_sub" {
+  count = "${length(local.az_names)}"
+  subnet_id = "${local.pub_sub_id[count.index]}"
+  route_table_id = "${aws_route_table.prt.id}"
 }
